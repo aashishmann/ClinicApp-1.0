ff141a01ca8f04a693a508d9962942ddda2d42e1
Aashish Mann <aashish.mann@snapdeal.com>
2015-06-23 12:56:49 +0530

    Forgot to add patient file in last commit

---
+++ src/main/java/com/springapp/mvc/entity/Patient.java
@@ -0,0 +1,152 @@
+package com.springapp.mvc.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by aashish on 3/6/15.
+ */
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+    int    id;
+    String firstname;
+    String lastname;
+    String mobile;
+    String landline;
+    String refferedBy;
+    String dependent;
+    String sex;
+    String address;
+    Integer    age;
+    String maritalStatus;
+    String occupation;
+    Integer    birthYear;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "firstname", nullable = false)
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Column(name = "lastname", nullable = false)
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Column(name = "mobile")
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Column(name = "landline")
+    public String getLandline() {
+        return landline;
+    }
+
+    public void setLandline(String landline) {
+        this.landline = landline;
+    }
+
+    @Column(name = "reffered_by")
+    public String getRefferedBy() {
+        return refferedBy;
+    }
+
+    public void setRefferedBy(String refferedBy) {
+        this.refferedBy = refferedBy;
+    }
+
+    @Column(name = "dependent")
+    public String getDependent() {
+        return dependent;
+    }
+
+    public void setDependent(String dependent) {
+        this.dependent = dependent;
+    }
+
+    @Column(name = "sex", nullable = false)
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Column(name = "address")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Column(name = "age")
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Column(name = "marital_status")
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    @Column(name = "occupation")
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    @Column(name = "birth_year")
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", mobile=" + mobile + ", landline=" + landline + ", refferedBy=" + refferedBy
+                + ", dependent=" + dependent + ", sex=" + sex + ", address=" + address + ", age=" + age + ", maritalStatus=" + maritalStatus + ", occupation=" + occupation
+                + ", birthYear=" + birthYear + "]";
+    }
+
+}
